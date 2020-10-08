dd2c4344ac0b7baa9e034e03e8e975ed6c9a762b
molusoj <olusoji200@gmail.com>
2020-10-08 16:09:15 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text("Diceeee"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatefulWidget {
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                setState ((){
+               randomNum();
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                setState ((){
+                randomNum();
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void randomNum(){
+    leftDiceNumber = Random().nextInt(6) +1;
+    rightDiceNumber = Random().nextInt(6) +1;
+  }
+}
